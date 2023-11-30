7120780d137363ee8d3ca57b0db4fcc8a6b0e407
omargr299 <100447246+omargr299@users.noreply.github.com>
2023-11-30 23:25:46 +0000

    game

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+import random
 
-from flask import Flask
-app = Flask(__name__)
+game = True
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+score =  0
+
+options = {
+    "r":"rock", 
+    "p":"paper", 
+    "s":"scissors"
+}
+
+while game:
+
+    # Game code
+
+    # rock, paper, scissors
+    # get user input
+    user = input("rock (r), paper (p) or scissors (s)? ")
+
+    if user not in list(options.keys()):
+        # invalid input
+        print("Invalid input, try again.")
+        continue
+
+    # get computer input
+    computer = random.choice(list(options.keys()))
+
+    print("You chose", options[user], "and the computer chose", options[computer])
+    # compare user and computer input
+    if user == computer:
+        print("It's a tie!")
+    elif user == "r" and computer == "s":
+        print("You win!")
+        score = score + 1
+    elif user == "p" and computer == "r":
+        print("You win!")
+        score = score + 1
+    elif user == "s" and computer == "p":
+        print("You win!")
+        score = score + 1
+    else:
+        print("You lose!")
+
+    # Ask the user if they want to play again
+    play_again = input("Do you want to play again? (y/n) ")
+
+    if play_again == "n":
+        game = False
+
+print("Your score is", score)
